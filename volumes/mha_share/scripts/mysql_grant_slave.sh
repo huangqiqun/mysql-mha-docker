727431e3c9d4b44cfc1bff7472b3816261688504
huangqiqun <huangqiqun@126.com>
2019-05-08 18:11:33 +0800

    修改mha管理账号

--- volumes/mha_share/scripts/mysql_grant_slave.sh
+++ volumes/mha_share/scripts/mysql_grant_slave.sh
@@ -1,5 +1,5 @@
 mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
-GRANT ALL ON *.* TO root@'10.5.0.%' IDENTIFIED BY 'root';
+GRANT ALL ON *.* TO admin@'10.5.0.%' IDENTIFIED BY 'admin';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'10.5.0.%' IDENTIFIED BY 'repl';
 reset master;
 EOSQL
